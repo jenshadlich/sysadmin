e05a5065ae768d547c4b76386e03645109ae4aa4
Jens Hadlich <jenshadlich@googlemail.com>
2015-03-30 21:43:17 +0200

    add check to run the script only as root

--- scripts/rpi/install_collectd.sh
+++ scripts/rpi/install_collectd.sh
@@ -1,17 +1,24 @@
 #/bin/sh
 
-# install
+# check for root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# install collectd
 apt-get install -y collectd-core
 
-# tmpfs for rrd files
+# use tmpfs for rrd files (used by rrdtool plugin)
 mount -t tmpfs -o size=16M none /var/lib/collectd/
 echo 'tmpfs    /var/lib/collectd/    tmpfs    defaults,size=16M      0       0' >> /etc/fstab
 
 # copy collectd.conf
 cp scripts/rpi/templates/collectd.conf /etc/collectd/collectd.conf
 
+# adjust hostname
 HOSTNAME="$(hostname)"
 sed -i "s/piXX/$HOSTNAME/g" /etc/collectd/collectd.conf
  
-# start
+# done
 service collectd start
