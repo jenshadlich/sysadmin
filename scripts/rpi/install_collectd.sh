74081b6791bf3abdd3da0744c898cfbdec639ff5
Jens Hadlich <jenshadlich@googlemail.com>
2015-03-30 20:42:19 +0200

    Update install_collectd.sh

--- scripts/rpi/install_collectd.sh
+++ scripts/rpi/install_collectd.sh
@@ -1,4 +1,14 @@
 #/bin/sh
+
+# install
 apt-get install -y collectd-core
+
+# tmpfs for rrd files
 mount -t tmpfs -o size=16M none /var/lib/collectd/
 echo 'tmpfs    /var/lib/collectd/    tmpfs    defaults,size=16M      0       0' >> /etc/fstab
+
+# copy collectd.conf
+cp templates/collectd.conf /etc/collectd/collectd.conf
+
+# start
+service collectd start
