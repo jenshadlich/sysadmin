e1a36348d107c858265e167040848de5b5c7b925
Jens Hadlich <jenshadlich@googlemail.com>
2015-03-30 21:05:21 +0200

    fix path

--- scripts/rpi/install_collectd.sh
+++ scripts/rpi/install_collectd.sh
@@ -8,7 +8,7 @@ mount -t tmpfs -o size=16M none /var/lib/collectd/
 echo 'tmpfs    /var/lib/collectd/    tmpfs    defaults,size=16M      0       0' >> /etc/fstab
 
 # copy collectd.conf
-cp templates/collectd.conf /etc/collectd/collectd.conf
+cp scripts/rpi/templates/collectd.conf /etc/collectd/collectd.conf
 
 HOSTNAME="$(hostname)"
 sed -i "s/piXX/$HOSTNAME/g" /etc/collectd/collectd.conf
